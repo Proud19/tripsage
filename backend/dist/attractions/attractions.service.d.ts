0edc0005cd30aa0e60a9d3ce16dc0e4c0253dc1c
Jason Chao <64825991+jchao01@users.noreply.github.com>
2024-04-30 15:47:57 -0700

    dist

--- backend/dist/attractions/attractions.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '@prisma/client';
-import { PrismaService } from 'src/prisma.service';
-export declare class AttractionsService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    getAttractions(): Promise<User[]>;
-}
