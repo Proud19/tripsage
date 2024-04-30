21cf1f17cc6888bb270ef8d6a32c5f4e912bab85
Jason Chao <64825991+jchao01@users.noreply.github.com>
2024-04-30 15:14:05 -0700

    prisma setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { UserModule } from './user/user.module';
+import { User } from './user/user';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, User],
 })
 export class AppModule {}
