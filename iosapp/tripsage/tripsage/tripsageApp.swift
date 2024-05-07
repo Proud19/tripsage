267d98eb64f023ceb6f2c3ee5324a6fd20eead0d
Proud Mpala <pmpala@stanford.edu>
2024-05-07 15:22:15 -0700

    added ios app

---
+++ iosapp/tripsage/tripsage/tripsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  tripsageApp.swift
+//  tripsage
+//
+//  Created by Proud Mpala on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct tripsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
