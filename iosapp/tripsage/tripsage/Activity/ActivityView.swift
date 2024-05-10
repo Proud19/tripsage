78dafb3a15204b4b27d8e8490edaae5deb704049
Beatriz Cunha Freire <biacf@stanford.edu>
2024-05-10 16:08:01 -0700

    [Fix] removed chat from explore and put it in activity tab

--- iosapp/tripsage/tripsage/Activity/ActivityView.swift
+++ iosapp/tripsage/tripsage/Activity/ActivityView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct ActivityView: View {
     var body: some View {
-        Text("Activity tab !")
+        HStack () {
+            Image(systemName: "person")
+                .frame(maxHeight: 32, alignment: .top)
+                .padding(.trailing, 4)
+            HStack () {
+                Text("Welcome to Salt Lake City!\nMy name is Sage, and I’ll be your tour guide today. I’ll be back when you’re near a landmark you might be interested in!").padding()
+            }
+            .frame(maxWidth: 260, alignment: .leading)
+            .background(.gray)
+            .cornerRadius(20)
+        }
+        .frame(maxWidth: 360, alignment: .leading)
     }
 }
 
--- iosapp/tripsage/tripsage/Explore/ExploreView.swift
+++ iosapp/tripsage/tripsage/Explore/ExploreView.swift
@@ -9,18 +9,7 @@ import SwiftUI
 
 struct ExploreView: View {
     var body: some View {
-        HStack () {
-            Image(systemName: "person")
-                .frame(maxHeight: 32, alignment: .top)
-                .padding(.trailing, 4)
-            HStack () {
-                Text("Welcome to Salt Lake City!\nMy name is Sage, and I’ll be your tour guide today. I’ll be back when you’re near a landmark you might be interested in!").padding()
-            }
-            .frame(maxWidth: 260, alignment: .leading)
-            .background(.gray)
-            .cornerRadius(20)
-        }
-        .frame(maxWidth: 360, alignment: .leading)
+        Text("Explore tab !")
     }
 }
 
