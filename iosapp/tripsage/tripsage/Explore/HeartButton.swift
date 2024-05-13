d8c62c1417ba66853ef33c32ac3759ddaf668a22
emma <emmawong@stanford.edu>
2024-05-13 13:18:16 -0700

    bouncy heart button

--- iosapp/tripsage/tripsage/Explore/HeartButton.swift
+++ iosapp/tripsage/tripsage/Explore/HeartButton.swift
@@ -8,19 +8,32 @@
 import SwiftUI
 
 struct HeartButton: View {
+    
     @State private var hearted = false
+    
     var body: some View {
         Button {
             self.hearted.toggle()
+        } label: {
+            ZStack {
+                image(Image(systemName: "heart.fill"), show: hearted)
+                image(Image(systemName: "heart"), show: !hearted)
+            }
         }
-    label: {
-        Image(systemName: hearted ? "heart.fill" : "heart")
-            .tint(hearted ? .red : .black)
-            .font(.system(size: 15))
     }
+
+        
+    func image(_ image: Image, show: Bool) -> some View {
+        image
+            .tint(hearted ? .red : .black)
+            .font(.system(size: 16))
+            .scaleEffect(show ? 1 : 0)
+            .opacity(show ? 1 : 0)
+            .animation(.interpolatingSpring(stiffness: 170, damping: 15), value: show)
     }
 }
 
 #Preview {
     HeartButton()
 }
+
