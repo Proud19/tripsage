ae223d56743eef76ec6ef815b8375850d0e76b63
Beatriz Cunha Freire <biacf@stanford.edu>
2024-05-21 22:52:55 -0700

    different layout for user and text box

--- iosapp/tripsage/tripsage/Activity/ActivityView.swift
+++ iosapp/tripsage/tripsage/Activity/ActivityView.swift
@@ -7,23 +7,58 @@
 
 import SwiftUI
 
+class ChatViewModel: ObservableObject {
+    
+    @Published var messages = [Message]()
+    
+    @Published var mockData = [
+        Message(userId: "12345", text: "Welcome to Salt Lake City!\nMy name is Sage, and I’ll be your tour guide today. I’ll be back when you’re near a landmark you might be interested in!", PhotoURL: "", createdAt: Date(), isFromUser: false),
+        Message(userId: "12345", text: "You are near the Natural History Museum of Utah, a museum as dramatic as the wonders it holds. There, you can learn about the natural world and the place of humans within it", PhotoURL: "", createdAt: Date(), isFromUser: false),
+        ]
+    
+    func sendMessage(text: String){
+        print(text)
+    }
+                
+}
+
 struct ActivityView: View {
+    @StateObject var chatViewModel = ChatViewModel()
+    @State var text = ""
     var body: some View {
-        HStack () {
-            Image(systemName: "person")
-                .frame(maxHeight: 32, alignment: .top)
-                .padding(.trailing, 4)
-            HStack () {
-                Text("Welcome to Salt Lake City!\nMy name is Sage, and I’ll be your tour guide today. I’ll be back when you’re near a landmark you might be interested in!").padding()
+        VStack {
+            ScrollView {
+                VStack(spacing:8){
+                    ForEach(chatViewModel.mockData){ message in
+                        MessageView(message: message)
+                    }
+                }
             }
-            .frame(maxWidth: 260, alignment: .leading)
-            .background(.gray)
-            .cornerRadius(20)
+            HStack {
+                TextField("Hello there", text: $text, axis: .vertical)
+                    .padding()
+                Button{
+                    if text.count >= 2{
+                        chatViewModel.sendMessage(text: text)
+                        chatViewModel.mockData.append(Message(userId: "12345", text: text, PhotoURL: "", createdAt: Date(), isFromUser: true))
+                        text = ""
+                    }
+                } label: {
+                    Text("Send")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.mint)
+                        .cornerRadius(50)
+                        .padding(.trailing)
+                }
+            }.background(Color(uiColor: .systemGray6))
         }
-        .frame(maxWidth: 360, alignment: .leading)
     }
 }
 
-#Preview {
-    ActivityView()
+struct ActivityView_Previews: PreviewProvider{
+    static var previews: some View{
+        ActivityView()
+    }
 }
+
---
+++ iosapp/tripsage/tripsage/Activity/MessageView.swift
@@ -0,0 +1,51 @@
+//
+//  MessageView.swift
+//  tripsage
+//
+//  Created by Beatriz Cunha Freire on 19/05/24.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var message: Message
+    
+    var body: some View {
+        if message.isFromUser {
+            HStack () {
+                HStack () {
+                    Text(message.text).padding()
+                }
+                .frame(maxWidth: 260, alignment: .topLeading)
+                .background(Color(uiColor: .systemBlue))
+                .cornerRadius(20)
+                
+                Image(systemName: "person")
+                    .frame(maxHeight: 32, alignment: .top)
+                    .padding(.trailing, 4)
+            }
+            .frame(maxWidth: 360, alignment: .trailing)
+        } else {
+            HStack () {
+                Image(systemName: "person")
+                    .frame(maxHeight: 32, alignment: .top)
+                    .padding(.trailing, 4)
+                HStack () {
+                    Text(message.text).padding()
+                }
+                .frame(maxWidth: 260, alignment: .leading)
+                .background(Color(uiColor: .lightGray))
+                .cornerRadius(20)
+            }
+            .frame(maxWidth: 360, alignment: .leading)
+        }
+            
+    }
+}
+
+struct MessageView_Preview: PreviewProvider{
+    static var previews: some View{
+        MessageView(message: Message(userId: "123", text: "hello", PhotoURL: "", createdAt: Date(), isFromUser: false))
+    }
+}
+
